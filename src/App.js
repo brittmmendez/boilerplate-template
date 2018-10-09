9e16e908315d465df710cba645377031e98cd68f
Brittany Morris <brittmmendez@gmail.com>
2018-10-09 13:37:49 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import DevTools from 'mobx-react-devtools';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <DevTools />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
